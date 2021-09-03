4f7057e84f9fe5b3e2407be7a65a32a9a439842d
Andres Aleman <aaleman5@jh.edu>
2021-09-02 21:01:59 -0400

    adding example_v4 functions

--- workspace/examples/src/examples.c
+++ workspace/examples/src/examples.c
@@ -20,8 +20,8 @@
 #define ENABLE_EXAMPLE_V0 (0)
 #define ENABLE_EXAMPLE_V1 (0)
 #define ENABLE_EXAMPLE_V2 (0)
-#define ENABLE_EXAMPLE_V3 (1)
-#define ENABLE_EXAMPLE_V4 (0)
+#define ENABLE_EXAMPLE_V3 (0)
+#define ENABLE_EXAMPLE_V4 (1)
 
 //Message Constraints
 #define MAX_MESSAGES      ( 2)
@@ -284,6 +284,181 @@ void do_example_v3(int startIndex, int stopIndex)
         char *exampleString1 = 0;
         getExampleString_v3(&msgInst, currentIndex, &exampleString1); //pass pointer of msgInst
         printf("%s\n\r", exampleString1);
+    }
+
+}
+#endif
+
+#if ENABLE_EXAMPLE_V4 == (1)
+//Example V4 Source Code
+
+/**
+ * @brief data structure to hold messages
+ *
+ * Contains information on the number of strings, the max messages allowed and max size of message
+ */
+typedef struct {
+    int maxMessages;    /**< Max number of messages allowed. */
+    int maxMessageSize; /**< Max size of message */
+    int numMessages;    /**< number of messages in the data structure */
+    char **messages;    /**< pointer of pointers (ie dynamic 2d array) */
+}messages_v4_t;
+
+/**
+ * @brief Initialize messages_v4_t variable
+ *
+ * Allocate memory for the dynamic 2d array
+ * @param messages_v4_t  *msgInst - pointer to struct containing messages
+ * @param maxMessages - number of messages we can hold
+ * @param maxMessageSize - max size of a message
+ * @return status = 0 failed, 1 success
+ */
+int init_messages_v4(messages_v4_t* msgInst, int maxMessages, int maxMessageSize){
+    int status = 1;//assume we are good
+
+    msgInst->maxMessages    = maxMessages;
+    msgInst->maxMessageSize = maxMessageSize;
+    msgInst->numMessages    = 0;
+
+    //References for allocating
+    //https://web.archive.org/web/20170831232159/https://www.techiedelight.com/dynamically-allocate-memory-for-2d-array/
+    //https://www.geeksforgeeks.org/dynamically-allocate-2d-array-c/
+
+    //allocate memory for pointers to other areas - this is number of messages
+    msgInst->messages = (char**)malloc(maxMessages*sizeof(char*));
+
+    //loop through and create an areas for the message.  The above pointers will point to this memory
+    for(int msgIndex = 0; msgIndex < maxMessages; msgIndex++){
+        msgInst->messages[msgIndex] = (char*)malloc(maxMessageSize*sizeof(char));
+
+        if(msgInst->messages == NULL){
+            status = 0;
+            break;
+        }
+    }
+
+    return status;
+}
+
+/**
+ * @brief Adds a message to a messages_v4_t type variable
+ * Increments the number of messages stored internally
+ *
+ * @param messages_v4_t  *msgInst - pointer to struct containing messages
+ * @param message - char array for the message to store
+ * @return status = 0 failed, 1 success
+ */
+int addMessage_v4(messages_v4_t * msgInst, const char * message){
+    int status = 0;
+
+    if (msgInst->numMessages < msgInst->maxMessages){
+    	status = 1; //we have enough room.. set status to ok
+
+    	//copy the message over.
+        strncpy(msgInst->messages[msgInst->numMessages], message, msgInst->maxMessageSize);
+
+        msgInst->numMessages++;  //increment the count for number of messages stored
+    }
+    return status;
+}
+
+
+/**
+ * @brief Assigns exampleStringPtr to a global variable based on even/odd input value
+ *
+ * If `number` is even, then `exampleStringPtr` will be assigned to the string
+ * at index 0 of the `msgInst->messages` array
+ *
+ * If `number` is odd, then `exampleStringPtr` will be assigned to the string
+ * at index 1 of the `msgInst->messages` array
+ *
+ * @param messages_v3_t  *msgInst - pointer to struct containing messages
+ * @param int number - used to select which string to assign to exampleStringPtr
+ * @param char **exampleStringPtr - pointer to a char*
+ * @return void
+ *
+ */
+int getExampleString_v4(messages_v4_t *msgInst, int number, char **exampleStringPtr)
+{
+    int status = 0;
+    if (number % 2 == 0)
+    {
+        //even number
+        *exampleStringPtr = msgInst->messages[0];
+    }
+    else
+    {
+        //odd number
+        *exampleStringPtr = msgInst->messages[1];
+    }
+
+    return status;
+}
+
+
+/**
+ * @brief Main entry point for Example v3
+ *
+ * Declares two local variable of type messages_v4_t to store messages.
+ * Passes the two different variables to getExampleString_v4 when called.
+ * Loops from startIndex and stopIndex calls getExampleString_v4 then prints a string
+ *
+ * @param startIndex
+ * @param stopIndex
+ * @return void
+ *
+ */
+void do_example_v4(int startIndex, int stopIndex)
+{
+    int currentIndex;
+    messages_v4_t msgInst_daytime;
+    messages_v4_t msgInst_nighttime;
+
+    int status = 0;
+    status = init_messages_v4(&msgInst_daytime, MAX_MESSAGES,  MAX_MESSAGE_SIZE);
+    if(status == 0){
+        return;//fail silently for this example
+    }
+
+    status = init_messages_v4(&msgInst_nighttime, MAX_MESSAGES,  MAX_MESSAGE_SIZE);
+    if(status == 0){
+        return;//fail silently for this example
+    }
+
+    status = addMessage_v4(&msgInst_daytime, "Hello Day Time World");
+    if(status == 0){
+            return;//fail silently for this example
+    }
+
+    status = addMessage_v4(&msgInst_daytime, "Hello Day Time Class\0");
+    if(status == 0){
+            return;//fail silently for this example
+    }
+
+    status = addMessage_v4(&msgInst_nighttime, "Hello Night Time World\0");
+    if(status == 0){
+            return;//fail silently for this example
+    }
+
+    status = addMessage_v4(&msgInst_nighttime, "Hello Night Time Class\0");
+
+    if(status == 0){
+            return;//fail silently for this example
+    }
+    printf("\n\n\rmsgInst_daytime %d\n\n\r", msgInst_daytime.numMessages);
+
+    for(currentIndex = startIndex; currentIndex <= stopIndex; currentIndex++)
+    {
+        char *exampleString1 = 0;
+        char *exampleString2 = 0;
+
+        //process with msgInst_daytime
+        status = getExampleString_v4(&msgInst_daytime, currentIndex, &exampleString1);
+        printf("%s\n\r", exampleString1);
+
+        //process with msgInst_nighttime
+        status = getExampleString_v4(&msgInst_nighttime, currentIndex, &exampleString2);
+        printf("%s\n\r", exampleString2);
     }
 
 }
